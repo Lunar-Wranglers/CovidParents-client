867f25e03e84afa926259f91d109eeef9c47f2bb
Matt Moore <Matt.M.Moore13@outlook.com>
2021-08-11 05:49:50 -0400

    Added the SignUp/SignIn components to the header file; Added names to the links for the navbar; added bootstrap navigation styling; began crafting the authenticated and unauthenticated options

--- src/components/header.js
+++ src/components/header.js
@@ -1,9 +1,23 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
+import SignUp from './SignUp'
+import SignIn from './SignIn'
 import SearchBar from './searchBar'
+
+import { Nav, Navbar, Dropdown, DropdownButton } from 'react-bootstrap'
 import { 
     LogoSearch
  } from './header.module.css'
+
+const authenticatedOptions = (
+    <Fragment>
+        <Nav.Link href=''>Logout</Nav.Link>
+    </Fragment>
+)
+
+const unauthenticatedOptions = (
+    
+)
 
 const Header = ({ children }) => {
     return (
@@ -15,15 +29,26 @@ const Header = ({ children }) => {
             <nav>
                 <ul>
                     <li>
-                        <Link to=''>Placeholder 1</Link>
+                        <Link to=''>About Covid-19</Link>
                     </li>
                     <li>
-                        <Link to=''>Placeholder 1</Link>
+                        <Link to=''>Learn Facts</Link>
                     </li>
                     <li>
-                        <Link to=''>Placeholder 1</Link>
+                        <Link to=''>Find A Vaccine</Link>
+                    </li>
+                    <li>
+                        <Link to=''>Get Tested</Link>
+                    </li>
+                    <li>
+                        <Link to=''>Data</Link>
+                    </li>
+                    <li>
+                        <Link to=''>Travel</Link>
                     </li>
                 </ul>
+                <SignUp />
+                <SignIn />
             </nav>
         </div>
     )
