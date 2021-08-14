3f7e02f653ef67ce863722a393f8abd0e977e1c9
Matt Moore <Matt.M.Moore13@outlook.com>
2021-08-14 16:48:19 -0400

    commented out the Row import; held off on deleting as I was unsure if I would use it in the future

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 //import { withRouter } from 'react-router-dom'
 
 import Form from 'react-bootstrap/Form'
-import Row from 'react-bootstrap/Row'
+// import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 import Modal from 'react-bootstrap/Modal'
